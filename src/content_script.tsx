822f69fc6601310e3a546221d00432d222ab775c
50ra4 <50ra4.dev.dev@gmail.com>
2022-06-30 23:20:52 +0900

    fix: format

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,24 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 const App = () => {
   return <span>rakumo-de-extension</span>;
-}
+};
 
 const renderRoot = () => {
-  const root = createRoot(
-    document.body.appendChild(document.createElement('div'))
-  )
+  const root = createRoot(document.body.appendChild(document.createElement('div')));
   root.render(
     <StrictMode>
       <App />
-    </StrictMode>
-  )
-}
+    </StrictMode>,
+  );
+};
 
 const main = () => {
   console.log('rakumo-de-extension main');
   renderRoot();
-}
+};
 
 main();
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 const Popup = () => {
   return (
     <div
       style={{
-        color: "red",
-        fontSize: "24px",
-        width: "320px",
-        height: "320px",
+        color: 'red',
+        fontSize: '24px',
+        width: '320px',
+        height: '320px',
       }}
     >
       rakumo-de-extension popup
@@ -16,8 +16,9 @@ const Popup = () => {
   );
 };
 
-createRoot(document.getElementById("root")!).render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Popup />
-  </StrictMode>
+  </StrictMode>,
 );
