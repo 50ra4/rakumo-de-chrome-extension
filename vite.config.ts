ef48587bd7402a3e6badaa52e0c318217c019c02
50ra4 <50ra4.dev.dev@gmail.com>
2022-07-02 17:12:23 +0900

    chore: rename manifest.config and changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json'
+import { defineConfig } from 'vite';
+import { crx } from '@crxjs/vite-plugin';
+import { resolve } from 'path';
+
+import manifest from './manifest.config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), crx({manifest})]
-})
+  build: {
+    rollupOptions: {
+      input: {
+        // output file at '/index.html'
+        welcome: resolve(__dirname, 'index.html'),
+        // output file at '/src/popup/index.html'
+        popup: resolve(__dirname, 'src/popup/index.html'),
+        devTools: resolve(__dirname, 'src/devTools/index.html'),
+        options: resolve(__dirname, 'src/options/index.html'),
+      },
+    },
+  },
+  plugins: [crx({ manifest })],
+});
