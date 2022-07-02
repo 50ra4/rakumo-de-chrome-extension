8362c6c19af80e0ed917dd96b7a605ad96eb8db5
50ra4 <50ra4.dev.dev@gmail.com>
2022-07-02 17:12:51 +0900

    change: /src directories

---
+++ src/devTools/index.tsx
@@ -0,0 +1,24 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const Popup = () => {
+  return (
+    <div
+      style={{
+        color: 'red',
+        fontSize: '24px',
+        width: '320px',
+        height: '320px',
+      }}
+    >
+      rakumo-de-extension devtools
+    </div>
+  );
+};
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Popup />
+  </StrictMode>,
+);
