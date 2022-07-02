f72f853f8ec4003b1a8e449244d69ffa22fc117c
50ra4 <50ra4.dev.dev@gmail.com>
2022-07-02 15:50:04 +0900

    add: testing-library packages

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from '@jest/types';
 
+/**
+ * https://zenn.dev/sprout2000/articles/182d3294fe4645
+ */
 const config: Config.InitialOptions = {
   roots: ['src'],
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
