a52c729881e83545492c22af865f469140afd4d9
50ra4 <50ra4.dev.dev@gmail.com>
2022-07-03 23:05:42 +0900

    chore: get active tabs test

--- src/background.ts
+++ src/background.ts
@@ -19,22 +19,30 @@ const fetchListContent = async (tabId?: number) => {
 //   exportRecord: 'exportRecord',
 // } as const;
 
-// chrome.contextMenus.create({
-//   id: Menus.exportRecord,
-//   title: '勤怠を出力する',
-//   contexts: ['all'],
-// });
+chrome.contextMenus.create({
+  id: 'rakumo-de-extension-tab-test',
+  title: 'タブの種類を出力する',
+  contexts: ['all'],
+});
 
-// chrome.contextMenus.onClicked.addListener((info, tab) => {
-//   switch (info.menuItemId) {
-//     case Menus.exportRecord:
-//       fetchListContent(tab?.id);
-//       break;
+const getTabs = async () => {
+  const lastFocusedWindowTabs = await chrome.tabs.query({ lastFocusedWindow: true });
+  const activeTabs = await chrome.tabs.query({ active: true });
+  const currentWindowTabs = await chrome.tabs.query({ currentWindow: true });
+  console.log({ lastFocusedWindowTabs, activeTabs, currentWindowTabs });
+};
 
-//     default:
-//       break;
-//   }
-// });
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case 'rakumo-de-extension-tab-test':
+      console.log('listen tab', tab);
+      getTabs();
+      break;
+
+    default:
+      break;
+  }
+});
 
 chrome.runtime.onMessage.addListener((_message, _, sendResponse) => {
   // TODO: Switch to different processing depending on the message
