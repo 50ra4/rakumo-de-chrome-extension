e52a637e899cb08e13c693f44ff1c3290d8d738c
50ra4 <50ra4.dev.dev@gmail.com>
2022-07-03 23:07:26 +0900

    chore: comment out contextMenus script

--- src/background.ts
+++ src/background.ts
@@ -14,35 +14,30 @@ const fetchListContent = async (tabId?: number) => {
   return head.result;
 };
 
-// TODO: add menus
-// const Menus = {
-//   exportRecord: 'exportRecord',
-// } as const;
+// chrome.contextMenus.create({
+//   id: 'rakumo-de-extension-tab-test',
+//   title: 'タブの種類を出力する',
+//   contexts: ['all'],
+// });
 
-chrome.contextMenus.create({
-  id: 'rakumo-de-extension-tab-test',
-  title: 'タブの種類を出力する',
-  contexts: ['all'],
-});
+// const getTabs = async () => {
+//   const lastFocusedWindowTabs = await chrome.tabs.query({ lastFocusedWindow: true });
+//   const activeTabs = await chrome.tabs.query({ active: true });
+//   const currentWindowTabs = await chrome.tabs.query({ currentWindow: true });
+//   console.log({ lastFocusedWindowTabs, activeTabs, currentWindowTabs });
+// };
 
-const getTabs = async () => {
-  const lastFocusedWindowTabs = await chrome.tabs.query({ lastFocusedWindow: true });
-  const activeTabs = await chrome.tabs.query({ active: true });
-  const currentWindowTabs = await chrome.tabs.query({ currentWindow: true });
-  console.log({ lastFocusedWindowTabs, activeTabs, currentWindowTabs });
-};
+// chrome.contextMenus.onClicked.addListener((info, tab) => {
+//   switch (info.menuItemId) {
+//     case 'rakumo-de-extension-tab-test':
+//       console.log('listen tab', tab);
+//       getTabs();
+//       break;
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  switch (info.menuItemId) {
-    case 'rakumo-de-extension-tab-test':
-      console.log('listen tab', tab);
-      getTabs();
-      break;
-
-    default:
-      break;
-  }
-});
+//     default:
+//       break;
+//   }
+// });
 
 chrome.runtime.onMessage.addListener((_message, _, sendResponse) => {
   // TODO: Switch to different processing depending on the message
