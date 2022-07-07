12345b633831bc41e7a619d50b5217458d211957
50ra4 <50ra4.dev.dev@gmail.com>
2022-07-07 23:35:50 +0900

    add: sort to toAttendanceRecords

--- src/utils/attendance.ts
+++ src/utils/attendance.ts
@@ -1,4 +1,4 @@
-import { getDay, getDate, getMonth, format } from 'date-fns';
+import { getDay, getDate, getMonth, format, compareAsc } from 'date-fns';
 import ja from 'date-fns/locale/ja';
 import { dateStringToDate, isMatchDateFormat, timeStringToMinute } from './date';
 import { AttendanceReportDocument } from './document';
@@ -8,42 +8,43 @@ export const toAttendanceRecords = (document: AttendanceReportDocument) => {
   const displayedMonth = dateStringToDate(document.displayedMonth, 'yyyy年 M月度');
   const month = getMonth(displayedMonth) + 1;
 
-  // TODO: 念の為、日付でソートする
-  return document.listContent.map(
-    ({
-      date: dateStr,
-      checkIn: checkInStr,
-      checkOut: checkOutStr,
-      breakTime,
-      workingTime,
-      holidayText = '',
-      ...rest
-    }) => {
-      const isHoliday = holidayText.startsWith('全休');
-      const isFirstDay = isMatchDateFormat(dateStr, 'M/d (EEEEE)');
-      const date = dateStringToDate(isFirstDay ? dateStr : `${month}/${dateStr}`, 'M/d (EEEEE)');
-      const dayOfWeek = getDay(date);
-      const dayOfMonth = getDate(date);
-      const checkIn = checkInStr ? dateStringToDate(checkInStr, 'H:mm') : undefined;
-      const checkOut = checkOutStr ? dateStringToDate(checkOutStr, 'H:mm') : undefined;
-      const breakTimeMinute = breakTime ? timeStringToMinute(breakTime) : undefined;
-      const workingTimeMinute = workingTime ? timeStringToMinute(workingTime) : undefined;
-      return {
-        ...rest,
-        month,
-        isHoliday,
-        holidayText,
-        isFirstDay,
-        date,
-        dayOfWeek,
-        dayOfMonth,
-        checkIn,
-        checkOut,
-        breakTimeMinute,
-        workingTimeMinute,
-      };
-    },
-  );
+  return document.listContent
+    .map(
+      ({
+        date: dateStr,
+        checkIn: checkInStr,
+        checkOut: checkOutStr,
+        breakTime,
+        workingTime,
+        holidayText = '',
+        ...rest
+      }) => {
+        const isHoliday = holidayText.startsWith('全休');
+        const isFirstDay = isMatchDateFormat(dateStr, 'M/d (EEEEE)');
+        const date = dateStringToDate(isFirstDay ? dateStr : `${month}/${dateStr}`, 'M/d (EEEEE)');
+        const dayOfWeek = getDay(date);
+        const dayOfMonth = getDate(date);
+        const checkIn = checkInStr ? dateStringToDate(checkInStr, 'H:mm') : undefined;
+        const checkOut = checkOutStr ? dateStringToDate(checkOutStr, 'H:mm') : undefined;
+        const breakTimeMinute = breakTime ? timeStringToMinute(breakTime) : undefined;
+        const workingTimeMinute = workingTime ? timeStringToMinute(workingTime) : undefined;
+        return {
+          ...rest,
+          month,
+          isHoliday,
+          holidayText,
+          isFirstDay,
+          date,
+          dayOfWeek,
+          dayOfMonth,
+          checkIn,
+          checkOut,
+          breakTimeMinute,
+          workingTimeMinute,
+        };
+      },
+    )
+    .sort((a, b) => compareAsc(a.date, b.date));
 };
 
 export type AttendanceRecord = ReturnType<typeof toAttendanceRecords>[number];
