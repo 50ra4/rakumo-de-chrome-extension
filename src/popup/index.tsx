61f1c2ae6c5a7010b639bbb34627b2861bc87320
50ra4 <50ra4.dev.dev@gmail.com>
2022-07-07 23:57:22 +0900

    add: working-time input to popup

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,6 +1,11 @@
 import React, { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import { generateCsv, toAttendanceRecords, generateTextPlain } from '../utils/attendance';
+import {
+  generateCsv,
+  toAttendanceRecords,
+  generateTextPlain,
+  toReportSummary,
+} from '../utils/attendance';
 import { AttendanceReportDocument } from '../utils/document';
 
 const OUTPUT_FORMAT_OPTIONS = [
@@ -20,6 +25,7 @@ type OutputFormat = typeof OUTPUT_FORMAT_OPTIONS[number];
 
 const Popup = () => {
   const [outputFormat, setOutputFormat] = useState<OutputFormat>(OUTPUT_FORMAT_OPTIONS[0]);
+  const [workingTime, setWorkingTime] = useState('');
 
   const onClickExport = () => {
     chrome.runtime.sendMessage<
@@ -41,6 +47,17 @@ const Popup = () => {
     });
   };
 
+  const onClickExpectedTime = () => {
+    chrome.runtime.sendMessage<
+      { name: 'message' },
+      { status: 'done'; data: AttendanceReportDocument }
+    >({ name: 'message' }, (response) => {
+      const summary = toReportSummary(response.data);
+      console.log(summary);
+      // TODO: calc times
+    });
+  };
+
   const onChangeFormat = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selected =
       OUTPUT_FORMAT_OPTIONS.find(({ type }) => e.target.value === type) ?? OUTPUT_FORMAT_OPTIONS[0];
@@ -54,7 +71,7 @@ const Popup = () => {
         minHeight: '320px',
       }}
     >
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: 'flex', marginBottom: '8px' }}>
         <select value={outputFormat.type} onChange={onChangeFormat}>
           {OUTPUT_FORMAT_OPTIONS.map(({ type, name }) => (
             <option key={type} value={type}>
@@ -64,6 +81,20 @@ const Popup = () => {
         </select>
         <button onClick={onClickExport}>勤怠情報を出力する</button>
       </div>
+      <div style={{ display: 'flex' }}>
+        <label htmlFor="working-time">1日の勤務時間</label>
+        <input
+          type="text"
+          id="working-time"
+          name="working-time"
+          placeholder="H:mm 形式で入力"
+          value={workingTime}
+          onChange={(e) => {
+            setWorkingTime(e.currentTarget.value);
+          }}
+        />
+      </div>
+      <button onClick={onClickExpectedTime}>予測時間を表示する</button>
     </div>
   );
 };
