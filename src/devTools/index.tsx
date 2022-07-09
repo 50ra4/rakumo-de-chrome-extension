1906dccf0d52841a2050fd4bfe8a443c33c877d8
50ra4 <50ra4.dev.dev@gmail.com>
2022-07-10 00:55:11 +0900

    add: chrome.devtools.network.onRequestFinished sample

--- src/devTools/index.tsx
+++ src/devTools/index.tsx
@@ -1,24 +1,28 @@
-import React, { StrictMode } from 'react';
+import React, { StrictMode, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 
-const Popup = () => {
-  return (
-    <div
-      style={{
-        color: 'red',
-        fontSize: '24px',
-        width: '320px',
-        height: '320px',
-      }}
-    >
-      rakumo-de-extension devtools
-    </div>
-  );
+const App = () => {
+  useEffect(() => {
+    chrome.devtools.network.onRequestFinished.addListener((details) => {
+      console.log(`watch`, details.request.url);
+
+      if (!/a-rakumo.appspot.com\/api\//.test(details.request.url)) {
+        return;
+      }
+
+      details.getContent((content, _) => {
+        const response = JSON.parse(content);
+        console.log(`content aaas ${details.request.url}`, response);
+      });
+    });
+  }, []);
+
+  return null;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Popup />
+    <App />
   </StrictMode>,
 );
