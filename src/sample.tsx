331ed5291be4177b21fa53155931dc14f5fa9fa7
50ra4 <50ra4.dev.dev@gmail.com>
2022-09-24 16:01:57 +0900

    add: Button to components

--- src/sample.tsx
+++ src/sample.tsx
@@ -1,10 +1,23 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Button } from './components/Button';
 
 const Root = () => {
   return (
     <div>
       <h1>rakumo-de-extension components samples</h1>
+      <section>
+        <h2>button</h2>
+        <div>
+          <Button
+            onClick={() => {
+              // nothing
+            }}
+          >
+            Button
+          </Button>
+        </div>
+      </section>
     </div>
   );
 };
